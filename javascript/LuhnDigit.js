f7abf1d323d5b3e4d2e6013cf90ed639d67032f6
Martin <mr.mmaurer+work@gmail.com>
2018-06-23 00:32:39 +0200

    Added aslant and created class

--- javascript/LuhnDigit.js
+++ javascript/LuhnDigit.js
@@ -1,40 +1,50 @@
-function getLuhnCheckDigit (number) {
-  var lastIndex = number.toString().length - 1 ;
-  var isOdd = true;
-  var sum = 0;
-  var curChar = '0';
-  var addValue = 0;
-  var multipliedDigit = 0;
-
-  number += "";
-
-  for (var n = lastIndex; n>=0; n--) {
-    curChar = number.charAt(n);
-
-    if (isOdd) {
-      multipliedDigit = curChar * 2;
-
-      if (multipliedDigit > 9) {
-        addValue = multipliedDigit - 9;
-      } else {
-        addValue = multipliedDigit;
-      }
-    } else {
-      addValue = curChar;
+class LuhnChecker {
+    constructor(num) {
+        this.num = num;
     }
-    isOdd = !isOdd;
-    sum += parseInt(addValue);
-  }
-  return ((10 - (sum % 10)) % 10);
+
+    getLuhnCheckDigit() {
+        var lastIndex = this.num.toString().length - 1 ;
+        var isOdd = true;
+        var sum = 0;
+        var curChar = '0';
+        var addValue = 0;
+        var multipliedDigit = 0;
+
+        this.num += "";
+
+        for (var n = lastIndex; n>=0; n--) {
+            curChar = this.num.charAt(n);
+
+            if (isOdd) {
+                multipliedDigit = curChar * 2;
+
+                if (multipliedDigit > 9) {
+                    addValue = multipliedDigit - 9;
+                } else {
+                    addValue = multipliedDigit;
+                }
+            } else {
+                addValue = curChar;
+            }
+            isOdd = !isOdd;
+            sum += parseInt(addValue);
+        }
+        return ((10 - (sum % 10)) % 10);
+    }
 }
 
 module.exports = function(number) {
-  return getLuhnCheckDigit(number);
+    const checker = new LuhnChecker(number);
+    const num = checker.getLuhnCheckDigit();
+
+    return num;
 }
 
 if (require.main === module) {
-  var number = process.argv.slice(2)[0];
-  var checkDigit = getLuhnCheckDigit(number);
+    const param = process.argv.slice(2)[0];
+    const checker = new LuhnChecker(param);
+    const num = checker.getLuhnCheckDigit();
 
-  console.log(checkDigit);
+    console.log(num);
 }
